1d61f4d287213d6e2c5037f5aa2e915e30d5c355
AnkitSingh <ankitankitksingh474@gmail.com>
2024-03-20 17:36:19 +0530

    Loginform

---
+++ Login-Form/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Loginform from './Components/loginform/Loginform';
+
+
+function App() {
+  return (
+    <div>
+      <Loginform/>
+    </div>
+  )
+}
+
+export default App;
